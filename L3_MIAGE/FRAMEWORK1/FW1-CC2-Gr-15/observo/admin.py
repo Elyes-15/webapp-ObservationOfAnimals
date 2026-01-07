60116b2cafcc0c729a15d2f5af8d7927061d455a
Hacini Kamilia <kamilia.hacini@etu.univ-orleans.fr#>
2026-01-07 20:12:52 +0100

    question 11 - creation du modele observation

--- L3_MIAGE/FRAMEWORK1/FW1-CC2-Gr-15/observo/admin.py
+++ L3_MIAGE/FRAMEWORK1/FW1-CC2-Gr-15/observo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Observation, Animal
+admin.site.register(Observation)
+admin.site.register(Animal)
 # Register your models here.
--- L3_MIAGE/FRAMEWORK1/FW1-CC2-Gr-15/observo/models.py
+++ L3_MIAGE/FRAMEWORK1/FW1-CC2-Gr-15/observo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    nom_commun = models.CharField(max_length=100)
+    nom_savant = models.CharField(max_length=200)
+    embranchement = models.CharField(max_length=100)
+    classe = models.CharField(max_length=100)
+    ordre = models.CharField(max_length=100)
+    sous_ordre = models.CharField(max_length=100)
+    famille = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    statut_iucn = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.nom_commun
+
+
+class Observation(models.Model):
+    date = models.DateField()
+    heure = models.TimeField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Observation de {self.animal.nom_commun} le {self.date}"
