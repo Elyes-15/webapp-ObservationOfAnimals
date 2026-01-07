9c94db17cb56f51cef8aa261c41ac3a27327abf9
Touak Rayane <rayane.touak@etu.univ-orleans.fr>
2026-01-07 19:20:34 +0100

    Question 4: Modèle Animal et admin

--- L3_MIAGE/FRAMEWORK1/FW1-CC2-Gr-15/observo/models.py
+++ L3_MIAGE/FRAMEWORK1/FW1-CC2-Gr-15/observo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    nom_commun = models.CharField(max_length=100)
+    nom_savant = models.CharField(max_length=100)
+    embranchement = models.CharField(max_length=50)
+    classe = models.CharField(max_length=50)
+    ordre = models.CharField(max_length=50)
+    sous_ordre = models.CharField(max_length=50, blank=True, null=True)
+    famille = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    statut_iucn = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.nom_commun
